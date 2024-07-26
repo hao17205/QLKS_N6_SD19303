0f50bb7b87e4e239be36f817ad9d4e99fb4fe1e8
Phí Phúc Danh <danhppph49417@gmail.com>
2024-07-26 15:23:05 +0700

    login

---
+++ QLKS/src/Model/Model_LG.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author PC
+ */
+public class Model_LG {
+
+    private String maTaiKhoan;
+    private String tenDangNhap;
+    private String matKhau;
+    private String chucVu;
+
+    public Model_LG(String tenDangNhap, String matKhau) {
+        this.tenDangNhap = tenDangNhap;
+        this.matKhau = matKhau;
+    }
+
+    public Model_LG() {
+    }
+
+    public Model_LG(String maTaiKhoan, String tenDangNhap, String matKhau, String chucVu) {
+        this.maTaiKhoan = maTaiKhoan;
+        this.tenDangNhap = tenDangNhap;
+        this.matKhau = matKhau;
+        this.chucVu = chucVu;
+    }
+
+    public String getMaTaiKhoan() {
+        return maTaiKhoan;
+    }
+
+    public void setMaTaiKhoan(String maTaiKhoan) {
+        this.maTaiKhoan = maTaiKhoan;
+    }
+
+    public String getTenDangNhap() {
+        return tenDangNhap;
+    }
+
+    public void setTenDangNhap(String tenDangNhap) {
+        this.tenDangNhap = tenDangNhap;
+    }
+
+    public String getMatKhau() {
+        return matKhau;
+    }
+
+    public void setMatKhau(String matKhau) {
+        this.matKhau = matKhau;
+    }
+
+    public String getChucVu() {
+        return chucVu;
+    }
+
+    public void setChucVu(String chucVu) {
+        this.chucVu = chucVu;
+    }
+
+    public Object[] toDataRow_LG(){
+        return new Object[]{this.getMaTaiKhoan(), this.getTenDangNhap(), this.getMatKhau(), this.getChucVu()};
+    }
+}
