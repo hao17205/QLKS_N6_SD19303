a5c8b2f4cb69a230838f52d4b2501ebee647dfed
DatFpoly05z <datlocanteam@gmail.com>
2024-07-27 13:27:47 +0700

    err

--- QLKS/src/view/view_DichVu.java
+++ QLKS/src/view/view_DichVu.java
@@ -51,9 +51,7 @@ public class view_DichVu extends javax.swing.JFrame {
 
         }
     }
-<<<<<<< HEAD
-    // haaaaaaaaaaaaaaaaaaaaaa
-=======
+
     
     private void fillTableDDV(ArrayList<Model_DichVu> list) {
         mol = (DefaultTableModel) tbl_DDV.getModel();
@@ -71,7 +69,7 @@ public class view_DichVu extends javax.swing.JFrame {
             mol.addRow(x.toDaTaRow_CTT());
     }
     }
->>>>>>> c56277f472acc8e641cb7ef27582ea0d05774a83
+
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
